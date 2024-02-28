03d8fc5b192aef839eecad032a132bcee9725e7a
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2024-02-28 12:33:29 +0000

    no need to change directory

--- .github/scripts/release_code.sh
+++ .github/scripts/release_code.sh
@@ -13,5 +13,4 @@ TRUSTSTORE_BUCKET_NAME=$(echo "${TRUSTSTORE_BUCKET_ARN}" | cut -d ":" -f 6)
 LATEST_TRUSTSTORE_VERSION=$(aws s3api list-object-versions --bucket "${TRUSTSTORE_BUCKET_NAME}" --prefix "${TRUSTSTORE_FILE}" --query 'Versions[?IsLatest].[VersionId]' --output text)
 export LATEST_TRUSTSTORE_VERSION
 
-cd ../../.aws-sam/build || exit
 make sam-deploy-package
