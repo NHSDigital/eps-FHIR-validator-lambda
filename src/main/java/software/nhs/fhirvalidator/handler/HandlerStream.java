09b69fa5eac715fe255474feb49061c9b94f34f6
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2024-03-05 08:30:07 +0000

    correct name

---
+++ src/main/java/software/nhs/fhirvalidator/handler/HandlerStream.java
@@ -0,0 +1,60 @@
+package software.nhs.fhirvalidator.handler;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import software.amazon.lambda.powertools.logging.Logging;
+import software.nhs.fhirvalidator.controller.ValidateController;
+import software.nhs.fhirvalidator.util.ResourceUtils;
+
+public class HandlerStream implements RequestStreamHandler {
+
+    private final ValidateController validateController;
+    Logger log = LogManager.getLogger(HandlerStream.class);
+
+    public HandlerStream() {
+        log.info("Creating the Validator instance for the first time...");
+
+        validateController = new ValidateController();
+
+        log.info("Validating once to force the loading of all the validator related classes");
+        String primerPayload = ResourceUtils.getResourceContent("primerPayload.json");
+        validateController.validate(primerPayload);
+
+        log.info("Validator is ready");
+    }
+
+    @Logging
+    @Override
+    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+        ByteArrayOutputStream result = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        try {
+            for (int length; (length = inputStream.read(buffer)) != -1;) {
+                result.write(buffer, 0, length);
+            }
+            String rawInput = result.toString("UTF-8");
+            log.info(rawInput);
+
+            String validatorResult = validateController.validate(rawInput);
+
+            try (PrintWriter writer = new PrintWriter(outputStream)) {
+                writer.print(validatorResult);
+                writer.close();
+            }
+
+        } catch (IOException ex) {
+            log.error(ex.getMessage(), ex);
+            throw new RuntimeException("error in handleRequest", ex);
+        }
+    }
+}
---
+++ src/main/java/software/nhs/fhirvalidator/util/OperationOutcomeUtils.java
@@ -0,0 +1,34 @@
+package software.nhs.fhirvalidator.util;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.hl7.fhir.r4.model.OperationOutcome;
+
+public class OperationOutcomeUtils {
+    public static OperationOutcome createOperationOutcome(String diagnostics, String expression) {
+        OperationOutcome.OperationOutcomeIssueComponent issue = createOperationOutcomeIssue(diagnostics, expression);
+        List<OperationOutcome.OperationOutcomeIssueComponent> issues = Collections.singletonList(issue);
+        return createOperationOutcome(issues);
+    }
+
+    public static OperationOutcome createOperationOutcome(
+            List<OperationOutcome.OperationOutcomeIssueComponent> issues) {
+        OperationOutcome operationOutcome = new OperationOutcome();
+        issues.forEach(operationOutcome::addIssue);
+        return operationOutcome;
+    }
+
+    public static OperationOutcome.OperationOutcomeIssueComponent createOperationOutcomeIssue(String diagnostics,
+            String expression) {
+        OperationOutcome.OperationOutcomeIssueComponent issue = new OperationOutcome.OperationOutcomeIssueComponent();
+        issue.setSeverity(OperationOutcome.IssueSeverity.ERROR);
+        issue.setCode(OperationOutcome.IssueType.PROCESSING);
+        issue.setDiagnostics(diagnostics);
+        if (expression != null) {
+            issue.addExpression(expression);
+        }
+        return issue;
+    }
+
+}
