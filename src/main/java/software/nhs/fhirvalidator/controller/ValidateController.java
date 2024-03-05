3037ae06ea5a8333e7a43764e72037dc9ee1e3a1
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2024-03-05 09:38:13 +0000

    fix if statement

--- src/main/java/software/nhs/fhirvalidator/controller/ValidateController.java
+++ src/main/java/software/nhs/fhirvalidator/controller/ValidateController.java
@@ -89,16 +89,19 @@ public class ValidateController {
             return new ArrayList<>();
         }
 
-        if (inputResource instanceof Bundle && ((Bundle) inputResource).getType() == Bundle.BundleType.SEARCHSET) {
-            List<IBaseResource> bundleResources = new ArrayList<>();
-            for (Bundle.BundleEntryComponent entry : ((Bundle) inputResource).getEntry()) {
-                bundleResources.add(entry.getResource());
+        if (inputResource instanceof Bundle) {
+            if (((Bundle) inputResource).getType() == Bundle.BundleType.SEARCHSET) {
+                List<IBaseResource> bundleResources = new ArrayList<>();
+                for (Bundle.BundleEntryComponent entry : ((Bundle) inputResource).getEntry()) {
+                    bundleResources.add(entry.getResource());
+                }
+
+                if (bundleResources.stream()
+                        .allMatch(resource -> ((Bundle) resource).getResourceType() == ResourceType.Bundle)) {
+                    return bundleResources;
+                }
             }
 
-            if (bundleResources.stream()
-                    .allMatch(resource -> ((Bundle) resource).getResourceType() == ResourceType.Bundle)) {
-                return bundleResources;
-            }
         }
 
         return List.of(inputResource);
