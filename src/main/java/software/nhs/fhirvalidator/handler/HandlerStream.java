65dd361ee948f61c160308e6ff5eda80c1ee53b4
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2024-04-15 09:42:10 +0000

    sonar

--- src/main/java/software/nhs/fhirvalidator/handler/HandlerStream.java
+++ src/main/java/software/nhs/fhirvalidator/handler/HandlerStream.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.nio.charset.StandardCharsets;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
