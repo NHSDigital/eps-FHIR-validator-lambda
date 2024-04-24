228a4ad449d045787ec04f8147879c7cb21a6708
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2024-04-24 09:17:11 +0000

    use stream to get request

--- src/test/java/software/nhs/fhirvalidator/HandlerTest.java
+++ src/test/java/software/nhs/fhirvalidator/HandlerTest.java
@@ -3,19 +3,18 @@ package software.nhs.fhirvalidator;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import nl.altindag.log.LogCaptor;
-import software.nhs.fhirvalidator.handler.HandleRequest;
+import software.nhs.fhirvalidator.handler.HandlerStream;
 
 import org.junit.jupiter.api.Test;
 
 class HandlerTest {
 
     @Test
-    void logInfoAndWarnMessages() {
-        LogCaptor logCaptor = LogCaptor.forClass(HandleRequest.class);
+    void logStartupMessage() {
+        LogCaptor logCaptor = LogCaptor.forClass(HandlerStream.class);
 
-        new HandleRequest();
+        new HandlerStream();
 
         assertTrue(logCaptor.getInfoLogs().contains("Validator is ready"));
-
     }
 }
